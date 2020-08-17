af2968e67026531607572a989ba60018f8e89342
ddanailov <dimityr.danailov@gmail.com>
2020-08-17 23:54:47 +0300

    /users REST API endpoint is supported.

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,30 @@
 
 import express from 'express'
 import './lib/cron'
+import db from './lib/db'
 
 const app = express()
 
 app.get('/v1', (_, res) => {
   res.status(200).send('v1 is up and running').end()
+})
+
+app.get('/users', async (_, res) => {
+  const users = await db.get('users').value()
+
+  res.json({
+    users,
+  })
+})
+
+app.get('/users/add', async (_, res, __) => {
+  // const twitterFollowers = await db.get('twitterFollowers')
+  // const instagramFollowers = await db.get('instagramFollowers')
+  /*
+  res.json({
+    twitterFollowers,
+    instagramFollowers,
+  })*/
 })
 
 app.get('/', (_, res) => {
