ff9a4e0f3e0a6abc04d868ebefcd3f921d408890
ddanailov <dimityr.danailov@gmail.com>
2020-08-18 21:21:22 +0300

    App module has three new declarations: AddUserComponent, ListUsersComponent, NavbarComponent.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,8 +6,13 @@ import {AppComponent} from './app.component'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {AppMaterialModule} from './app-material.module'
 
+// Users
+import {AddUserComponent} from './add-user/add-user.component'
+import {ListUsersComponent} from './list-users/list-users.component';
+import { NavbarComponent } from './navbar/navbar.component'
+
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AddUserComponent, ListUsersComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
