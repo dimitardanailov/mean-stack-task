b75edaeabba817e723ea389eefa2f57fb7c7ef5e
ddanailov <dimityr.danailov@gmail.com>
2020-08-18 23:19:18 +0300

    The component is able to fetch user data.

--- frontend/src/app/list-users/list-users.component.ts
+++ frontend/src/app/list-users/list-users.component.ts
@@ -1,74 +1,5 @@
 import {Component, OnInit} from '@angular/core'
-
-export interface User {
-  firstName: string
-  lastName: string
-  email: string
-  role: string
-}
-
-const DUMMY_USERS: User[] = [
-  {
-    firstName: 'Default',
-    lastName: 'Defaultson',
-    email: 'def@somemail.com',
-    role: 'Art Manager',
-  },
-  {
-    firstName: 'Albert',
-    lastName: 'Einstein',
-    email: 'def@somemail.com',
-    role: 'Artist',
-  },
-  {
-    firstName: 'Arnold',
-    lastName: 'Schwarzenegger',
-    email: 'def@somemail.com',
-    role: 'Designer',
-  },
-  {
-    firstName: 'Success',
-    lastName: 'Moe',
-    email: 'john@example.com',
-    role: 'Jr. Developer',
-  },
-  {
-    firstName: 'Danger',
-    lastName: 'moe',
-    email: 'mary@example.com',
-    role: 'Developer',
-  },
-  {
-    firstName: 'Info',
-    lastName: 'Dooley',
-    email: 'july@example.com',
-    role: 'Artist',
-  },
-  {
-    firstName: 'Bill',
-    lastName: 'Clinton',
-    email: 'july@example.com',
-    role: 'Product Owner',
-  },
-  {
-    firstName: 'Warning',
-    lastName: 'Refs',
-    email: 'bo@example.com',
-    role: 'Sys Admin',
-  },
-  {
-    firstName: 'Active',
-    lastName: 'Activeson',
-    email: 'act@example.com',
-    role: 'Artist',
-  },
-  {
-    firstName: 'Penelope',
-    lastName: 'Cruz',
-    email: 'act@example.com',
-    role: 'Artist',
-  },
-]
+import User from '../../models/User'
 
 @Component({
   selector: 'app-list-users',
@@ -84,9 +15,13 @@ export class ListUsersComponent implements OnInit {
     'edit',
     'remove',
   ]
-  dataSource = DUMMY_USERS
+  users: User[] = []
 
   constructor() {}
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<any> {
+    const res = await fetch('http://localhost:8080/users')
+    const data = await res.json()
+    this.users = data.users
+  }
 }
