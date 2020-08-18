7817add4b98b4f5f2af9265e7601d7122203c641
ddanailov <dimityr.danailov@gmail.com>
2020-08-18 22:35:16 +0300

    Component has User defination, DUMMY_USERS and required ng table configurations.

--- frontend/src/app/list-users/list-users.component.ts
+++ frontend/src/app/list-users/list-users.component.ts
@@ -1,15 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+
+export interface User {
+  firstName: string
+  lastName: string
+  email: string
+  role: string
+}
+
+const DUMMY_USERS: User[] = [
+  {
+    firstName: 'Default',
+    lastName: 'Defaultson',
+    email: 'def@somemail.com',
+    role: 'Art Manager',
+  },
+  {
+    firstName: 'Albert',
+    lastName: 'Einstein',
+    email: 'def@somemail.com',
+    role: 'Artist',
+  },
+  {
+    firstName: 'Arnold',
+    lastName: 'Schwarzenegger',
+    email: 'def@somemail.com',
+    role: 'Designer',
+  },
+  {
+    firstName: 'Success',
+    lastName: 'Moe',
+    email: 'john@example.com',
+    role: 'Jr. Developer',
+  },
+  {
+    firstName: 'Danger',
+    lastName: 'moe',
+    email: 'mary@example.com',
+    role: 'Developer',
+  },
+  {
+    firstName: 'Info',
+    lastName: 'Dooley',
+    email: 'july@example.com',
+    role: 'Artist',
+  },
+  {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    email: 'july@example.com',
+    role: 'Product Owner',
+  },
+  {
+    firstName: 'Warning',
+    lastName: 'Refs',
+    email: 'bo@example.com',
+    role: 'Sys Admin',
+  },
+  {
+    firstName: 'Active',
+    lastName: 'Activeson',
+    email: 'act@example.com',
+    role: 'Artist',
+  },
+  {
+    firstName: 'Penelope',
+    lastName: 'Cruz',
+    email: 'act@example.com',
+    role: 'Artist',
+  },
+]
 
 @Component({
   selector: 'app-list-users',
   templateUrl: './list-users.component.html',
-  styleUrls: ['./list-users.component.css']
+  styleUrls: ['./list-users.component.css'],
 })
 export class ListUsersComponent implements OnInit {
+  displayedColumns: string[] = [
+    'firstName',
+    'lastName',
+    'email',
+    'role',
+    'edit',
+    'remove',
+  ]
+  dataSource = DUMMY_USERS
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
