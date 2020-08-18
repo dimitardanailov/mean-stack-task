0aae85e69a93739906f21192bff564c2df5bafd8
ddanailov <dimityr.danailov@gmail.com>
2020-08-18 22:06:21 +0300

    Component has onSubmit click listener.

--- frontend/src/app/add-user/add-user.component.ts
+++ frontend/src/app/add-user/add-user.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.css']
+  styleUrls: ['./add-user.component.css'],
 })
 export class AddUserComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit(f: NgForm) {
+    console.log(f.value) // { first: '', last: '' }
+    console.log(f.valid) // false
   }
-
 }
