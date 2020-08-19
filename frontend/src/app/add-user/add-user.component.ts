a7000800975ba85ff8e7f966232f7c38c4495214
ddanailov <dimityr.danailov@gmail.com>
2020-08-19 17:30:41 +0300

    Component is able to detect if Artist Manager is avaiable.

--- frontend/src/app/add-user/add-user.component.ts
+++ frontend/src/app/add-user/add-user.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core'
 import {NgForm} from '@angular/forms'
 import User from '../../models/User'
+import {environment} from '../../environments/environment'
 
 @Component({
   selector: 'app-add-user',
@@ -8,14 +9,35 @@ import User from '../../models/User'
   styleUrls: ['./add-user.component.css'],
 })
 export class AddUserComponent implements OnInit {
-  managerPositionIsAvailable = true
+  dbHasArtManager = false
 
   constructor() {}
 
-  ngOnInit(): void {}
+  async ngOnInit(): Promise<any> {
+    this.dbHasArtManager = await this.updateDbHasArtManagerProperty()
+    console.log('dbHasArtManager', this.dbHasArtManager)
+  }
 
   onSubmit(f: NgForm) {
     console.log(f.value) // { first: '', last: '' }
     console.log(f.valid) // false
+  }
+
+  async updateDbHasArtManagerProperty(): Promise<any> {
+    const promise = new Promise((resolve, reject) => {
+      fetch(environment.REST_API.roleArtManagerIsAvailable)
+        .then(async res => {
+          const data = await res.json()
+          resolve(data.dbHasArtManager)
+        })
+        .catch(error => {
+          console.error(
+            `${environment.REST_API.roleArtManagerIsAvailable} is unreachable`,
+          )
+          reject(error)
+        })
+    })
+
+    return promise
   }
 }
