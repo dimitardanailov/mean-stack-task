859628f6159ce295a1d3380f748916a1dd61e569
ddanailov <dimityr.danailov@gmail.com>
2020-08-19 22:12:47 +0300

    environment setup supports a new REST API: creating a new user.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -9,6 +9,8 @@ export const environment = {
   REST_API: {
     list_users: `${REST_API_SERVER}/users`,
     roleArtManagerIsAvailable: `${REST_API_SERVER}/users/role-art-manager-is-available`,
+    emailIsUnique: `${REST_API_SERVER}/users/email-is-unique`,
+    createUser: `${REST_API_SERVER}/users`,
   },
 }
 
