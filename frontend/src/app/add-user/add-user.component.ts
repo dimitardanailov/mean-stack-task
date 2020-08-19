2022f74214c3de36e43297a8175dd3604eb106ba
ddanailov <dimityr.danailov@gmail.com>
2020-08-19 22:14:18 +0300

    Component is able to create a new dummy user.

--- frontend/src/app/add-user/add-user.component.ts
+++ frontend/src/app/add-user/add-user.component.ts
@@ -10,6 +10,7 @@ import {environment} from '../../environments/environment'
 })
 export class AddUserComponent implements OnInit {
   dbHasArtManager = false
+  formErrorMessage = ''
 
   constructor() {}
 
@@ -17,7 +18,42 @@ export class AddUserComponent implements OnInit {
     this.dbHasArtManager = await this.updateDbHasArtManagerProperty()
   }
 
-  onSubmit(f: NgForm) {
+  async onSubmit(f: NgForm) {
+    const user: User = {
+      firstName: 'Dimitar',
+      lastName: 'Danailov',
+      email: 'dimityr.danailov@gmail.com',
+      role: 'designer',
+    }
+
+    if (user.role === 'art_manager') {
+      this.dbHasArtManager = await this.updateDbHasArtManagerProperty()
+      if (this.dbHasArtManager) {
+        this.formErrorMessage = 'Please choise a different position!'
+        return
+      }
+    }
+
+    const emailCanBeUsed = await this.emailIsUnique(user.email)
+    if (!emailCanBeUsed) {
+      this.formErrorMessage = "The current email can't be used!"
+      return
+    }
+
+    console.log('json', JSON.stringify(user))
+
+    const restAPIRequest = fetch(environment.REST_API.createUser, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+
+    restAPIRequest.then(res => {
+      console.log('response', res)
+    })
+
     console.log(f.value) // { first: '', last: '' }
     console.log(f.valid) // false
   }
@@ -30,9 +66,36 @@ export class AddUserComponent implements OnInit {
           resolve(data.dbHasArtManager)
         })
         .catch(error => {
+          /**
+           * DRY Anti pattern!!!
+           *
+           * TODO: The error handling should be reusable method.
+           */
           console.error(
             `${environment.REST_API.roleArtManagerIsAvailable} is unreachable`,
           )
+          reject(error)
+        })
+    })
+
+    return promise
+  }
+
+  async emailIsUnique(email): Promise<any> {
+    const uri = `${environment.REST_API.emailIsUnique}/${email}`
+    const promise = new Promise((resolve, reject) => {
+      fetch(uri)
+        .then(async res => {
+          const data = await res.json()
+          resolve(data.emailIsUnique)
+        })
+        .catch(error => {
+          /**
+           * DRY Anti pattern!!!
+           *
+           * TODO: The error handling should be reusable method.
+           */
+          console.error(`${uri} is unreachable`)
           reject(error)
         })
     })
