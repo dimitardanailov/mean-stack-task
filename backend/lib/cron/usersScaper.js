e890c479669bc17a5682a995465c11ba3f92dbfa
ddanailov <dimityr.danailov@gmail.com>
2020-08-19 18:14:33 +0300

    usersScaper emulates csv import or json import.

--- backend/lib/cron/usersScaper.js
+++ backend/lib/cron/usersScaper.js
@@ -6,6 +6,10 @@ async function runCron() {
   const users = await db.get('users').value()
   if (users.length > 0) return
 
+  /**
+   * CSV import is a common feature for users import.
+   * The current workflow emulates csv import or json import .
+   */
   const dummyUsers = uniqueEmails(dummyData.users)
 
   dummyUsers.forEach(user => {
