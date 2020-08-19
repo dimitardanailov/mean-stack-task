964a0d41cf0a05189ce3226d4c7ec8cd1793001c
ddanailov <dimityr.danailov@gmail.com>
2020-08-19 17:04:17 +0300

    Setup supports REST API configuration. The first configuration is available: list_users.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,9 +2,14 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+const REST_API_SERVER = 'http://localhost:8080'
+
 export const environment = {
-  production: false
-};
+  production: false,
+  REST_API: {
+    list_users: `${REST_API_SERVER}/users`,
+  },
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
