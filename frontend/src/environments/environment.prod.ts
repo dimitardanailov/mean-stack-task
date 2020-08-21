208b96b29edeb9d5a71ae651850ee587907cc462
ddanailov <dimityr.danailov@gmail.com>
2020-08-21 22:10:30 +0300

    environments configurations support updateUser REST API.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -7,6 +7,7 @@ export const environment = {
     roleArtManagerIsAvailable: `${REST_API_SERVER}/users/role-art-manager-is-available`,
     emailIsUnique: `${REST_API_SERVER}/users/email-is-unique`,
     createUser: `${REST_API_SERVER}/users`,
+    updateUser: `${REST_API_SERVER}/users`,
     deleteUser: `${REST_API_SERVER}/users`,
   },
 }
