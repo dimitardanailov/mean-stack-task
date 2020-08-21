223163979e6df86e2431391eafc04f008c916467
ddanailov <dimityr.danailov@gmail.com>
2020-08-21 12:05:52 +0300

    The end user is available navigate between different options.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(private router: Router) {}
+
+  navigate(url, sidenav) {
+    sidenav.toggle()
+    this.router.navigate([url])
+  }
 }
